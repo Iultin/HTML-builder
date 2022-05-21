2ea8e9fbdf5ebb49018aa875e7d1ef5da6abbd8a
Iultin <iultin@mail.ru>
2022-05-21 13:55:58 +0300

    feat: added writing to file (Sat, May 21, 2022 01:55:58 PM)

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 const {stdin, stdout} = process;
+
+let file = fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf8')
 stdout.write('Введите Ваше имя\n');
 //stdin.on('data', data => stdout.write(data));
 
@@ -9,8 +11,14 @@ stdin.on('data', dat => {
   if (dat == 'exit') process.exit();
   stdout.write('Привет, ');
   stdout.write(dat);
+  file.write(dat);
   stdout.write('!');
   stdout.write('\n');
-  process.on('SIGINT', handle);
+  // process.on('SIGINT', handle);
+  file.end('Заверешение записи');
+  // process.exit();
 });
-process.on('exit', () => stdout.write('Удачи в изучении Node.js!'));
+process.on('exit', () => {
+  stdout.write('Удачи в изучении Node.js!')
+} ); 
+  
