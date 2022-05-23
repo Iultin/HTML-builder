b5829336ff6cd9f00363b11536dd570248b5a870
Iultin <iultin@mail.ru>
2022-05-23 18:02:11 +0300

    feat: started task HTML-builder/04-copy-directory/ (Mon, May 23, 2022 06:02:11 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,7 +2,15 @@ const fs = require('fs/promises');
 const path = require('path');
 
 
-let folder = path.join(__dirname, 'secret-folder');
+let folder = path.join(__dirname, 'files1');
+
+async function copyFiles() {
+  // let ask = await fs.access(folder)
+  // console.log(ask);
+  await fs.mkdir(folder)
+}
+
+copyFiles();
 
 // async function getFiles(pathToFolder) {
 //   const dir = await fs.readdir(pathToFolder, { withFileTypes: true });
