3049fefe9c65b109698b6c11cff911a9901afc11
Iultin MyLapWork <iultin@mail.ru>
2022-05-24 15:04:09 +0300

    feat: added functions deleteFiles and initFolders (Tue, May 24, 2022 03:04:09 PM)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,3 +1,41 @@
 let text = 'Уважаемый проверяющий, если есть возможность, то проверьте это задание позже! Заранее спасибо!';
 
 console.log(text);
+
+const fs = require('fs');
+const path = require('path');
+
+let folderInt = path.join(__dirname, 'assets');
+let folderTo = path.join(__dirname, 'project-dist');
+
+async function deleteFiles(folder) {
+  // console.log(folder);
+  await fs.readdir(folder, { withFileTypes: true }, (err, files) => {
+    // console.log(files);
+    // if (err) throw err;
+    for (let file of files) {
+      if (file.isFile()) fs.unlink(path.resolve(folder, file.name), err => err);
+    }
+  });
+}
+
+async function initFolders() {
+  try {
+    await fs.mkdir(folderTo, err => err);
+    // console.log(11);
+    try {
+      await deleteFiles(folderTo);
+    } catch (err) {
+      // if (err) throw err;
+    }
+  } catch (err) {
+    if (err) throw err;
+    try {
+      await deleteFiles(folderTo);
+    } catch (err) {
+      // if (err) throw err;
+    }
+  }
+}
+initFolders();
+
