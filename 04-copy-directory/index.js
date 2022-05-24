7806fe3a3253e40722d63d7cb8a0701c383b4a7b
Iultin MyLapWork <iultin@mail.ru>
2022-05-24 10:40:53 +0300

    feat: added solution for task HTML-builder/04-copy-directory/ (Tue, May 24, 2022 10:40:53 AM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,15 +2,42 @@ const fs = require('fs/promises');
 const path = require('path');
 
 
-let folder = path.join(__dirname, 'files1');
+let folderInt = path.join(__dirname, 'files');
+let folderTo = path.join(__dirname, 'files-copy');
 
-async function copyFiles() {
-  // let ask = await fs.access(folder)
-  // console.log(ask);
-  await fs.mkdir(folder)
+async function initFolder() {
+  try {
+    await fs.mkdir(folderTo);
+    await copyFiles();
+  } catch (err) {
+    try {
+      // console.log(folderTo);
+      let files = await fs.readdir(folderTo);
+      for (let file of files) {
+        fs.unlink(path.join(folderTo, file));
+      }
+      await fs.rmdir(folderTo);
+      await fs.mkdir(folderTo);
+      await copyFiles();
+    } catch {
+      await fs.rmdir(folderTo);
+      await fs.mkdir(folderTo);
+      await copyFiles();
+    }
+  }
+
 }
 
-copyFiles();
+initFolder();
+
+async function copyFiles() {
+  let files = await fs.readdir(folderInt);
+  for (let file of files) {
+    fs.copyFile(path.join(folderInt, file), path.join(folderTo, file));
+  }
+}
+
+
 
 // async function getFiles(pathToFolder) {
 //   const dir = await fs.readdir(pathToFolder, { withFileTypes: true });
