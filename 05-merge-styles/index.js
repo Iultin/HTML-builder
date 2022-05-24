397083572b428ae407ddde533c097d6c4b28bd5a
Iultin MyLapWork <iultin@mail.ru>
2022-05-24 11:42:34 +0300

    feat: added solution for task HTML-builder/05-merge-styles/ (Tue, May 24, 2022 11:42:34 AM)

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,3 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 
+let folderInt = path.join(__dirname, 'styles');
+let folderTo = path.join(__dirname, 'project-dist');
+let fileBundle = fs.createWriteStream(path.join(folderTo, 'bundle.css'));
+let data;
+
+async function createBundle() {
+  await fs.readdir(folderInt, (err, files) => {
+    if (err) console.log(err);
+    for (let file of files) {
+      if (path.extname(file) === '.css') {
+        data = fs.createReadStream(path.resolve(folderInt, file));
+        data.on('data', function (chunk) {
+          fileBundle.write(chunk);
+          // console.log(chunk);
+        });
+      }
+    }
+
+  });
+  
+}
+
+createBundle();
