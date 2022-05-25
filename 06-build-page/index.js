d94358e9195a4a60c6992134080c8cb6ededdfbe
Iultin MyLapWork <iultin@mail.ru>
2022-05-25 09:49:13 +0300

    feat: added function createCSS (Wed, May 25, 2022 09:49:13 AM)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,4 +1,6 @@
-let text = 'Уважаемый проверяющий, если есть возможность, то проверьте это задание позже! Заранее спасибо!';
+let text = `Уважаемый проверяющий, если есть возможность, то проверьте это задание позже! 
+Сегодня после обеда.. 
+Заранее спасибо!`;
 
 console.log(text);
 
@@ -8,6 +10,16 @@ const path = require('path');
 
 let folderInt = path.join(__dirname, 'assets');
 let folderTo = path.join(__dirname, 'project-dist');
+
+let cssInt = path.join(__dirname, 'styles');
+let cssTo = path.join(__dirname, 'project-dist');
+// let cssBundle = fs.createWriteStream(path.join(cssTo, 'style.css'));
+let cssBundle;
+let htmlInt = path.join(__dirname, 'components');
+let htmlTo = path.join(__dirname, 'project-dist');
+// let htmlBundle = fs.createWriteStream(path.join(htmlTo, 'index.html'));
+let htmlBundle;
+
 
 async function deleteFiles(folder) {
   // console.log(folder);
@@ -22,8 +34,7 @@ async function deleteFiles(folder) {
 
 async function initFolders() {
   try {
-    await fs.mkdir(folderTo, err => err);
-    // console.log(11);
+    await fs.mkdir(folderTo, err => err);    // console.log(11);
     try {
       await deleteFiles(folderTo);
     } catch (err) {
@@ -38,12 +49,28 @@ async function initFolders() {
     }
   }
 }
-initFolders();
+
+async function createCSS() {
+  let data;
+  await fs.readdir(cssInt, (err, files) => {
+    if (err) console.log(err);
+    for (let file of files) {
+      if (path.extname(file) === '.css') {
+        data = fs.createReadStream(path.resolve(cssInt, file));
+        data.on('data', function (chunk) {
+          cssBundle.write(chunk);
+          // console.log(chunk);
+        });
+      }
+    }
+  });
+}
+
 
 async function copyFiles(src, dest) {
-  await fsPromises.rm(dest, {recursive: true, force: true});
-  await fsPromises.mkdir(dest, {recursive: true});
-  const files = await fsPromises.readdir(src, {withFileTypes: true});
+  await fsPromises.rm(dest, { recursive: true, force: true });
+  await fsPromises.mkdir(dest, { recursive: true });
+  const files = await fsPromises.readdir(src, { withFileTypes: true });
   for (let file of files) {
     let pathInt = path.join(src, file.name);
     let pathTo = path.join(dest, file.name);
@@ -52,8 +79,13 @@ async function copyFiles(src, dest) {
     }
     else {
       await fsPromises.copyFile(pathInt, pathTo);
+      cssBundle = await fs.createWriteStream(path.join(cssTo, 'style.css'));
+      htmlBundle = fs.createWriteStream(path.join(htmlTo, 'index.html'));
+      await createCSS();
     }
   }
 }
 
+initFolders();
 copyFiles(folderInt, folderTo);
+
